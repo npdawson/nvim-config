399c144405eee99790099a931f1cc164f3203b5e
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2024-01-07 11:41:45 -0700

    change cmp confirm from CR to C-y

--- init.lua
+++ init.lua
@@ -626,11 +626,8 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
   },
   sources = {
     { name = 'nvim_lsp' },
