1d08e49b2c3e765b3650511ff7f766c506046e7a
Natalie <natalie.p.dawson@gmail.com>
2025-05-15 12:36:03 -0700

    Update telescope.lua
    Change telescope-fzf-native to use cmake
    Add telescope-ui-select

--- lua/natalie/plugins/telescope.lua
+++ lua/natalie/plugins/telescope.lua
@@ -6,15 +6,12 @@ return {
 			"nvim-lua/plenary.nvim",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+				cond = function ()
+					return vim.fn.executable "cmake" == 1
 				end,
 			},
+			"nvim-telescope/telescope-ui-select.nvim",
 		},
-		config = function()
-			require("telescope").setup({})
-			require("telescope").load_extension("fzf")
-		end,
 	},
 }
